479933981a25605148cb638c791777856a1b31ce
doctoc30 <fazhoq1977@gmail.com>
2026-06-24 04:56:06 +0100

    Import OneSignal SDK and update fetch event handler

--- sw.js
+++ sw.js
@@ -1,3 +1,6 @@
+importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
+
+/* Your original PWA service worker code */
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => e.waitUntil(clients.claim()));
 self.addEventListener('fetch', e => e.respondWith(fetch(e.request)));
